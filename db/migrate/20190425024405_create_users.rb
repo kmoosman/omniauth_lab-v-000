6d6646d541a810d9d83d51badc92eea89ec36f77
Katie Moosman <moosman.katie@gmail.com>
2019-04-25 03:20:12 +0000

    Automatically backed up by Learn

--- db/migrate/20190425024405_create_users.rb
+++ db/migrate/20190425024405_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :name
       t.string :email
       t.string :image
+      t.integer :uid
       t.string :password_digest
       t.timestamps
     end
